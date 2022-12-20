d770766898cbd8125ccc6b5033bb0e514301ad1b
danilogv <danilo.gv@hotmail.com>
2022-12-20 11:58:41 -0300

    Incluíndo biblioteca de navegação stack

---
+++ front-end/src/empresa/ListaEmpresas.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {SafeAreaView,Text} from "react-native";
+
+export default function ListaEmpresas() {
+    return (
+        <SafeAreaView>
+            <Text>Olá Mundo!</Text>
+        </SafeAreaView>
+    );
+}
